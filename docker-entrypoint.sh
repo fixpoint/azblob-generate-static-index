59a6d7e5825a7ae17044f45a40d0dd67cb1e8ed4
Alisue <lambdalisue@gmail.com>
2022-10-24 20:35:32 +0900

    Refactor `docker-entrypoint.sh`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+CONTAINER_STRING=$1
+CONTAINER_NAME=$2
+PREFIX=$3
 
 set -Cue
 
+if [[ -z $CONTAINER_STRING ]]; then
+  echo "container string is not specified" >&2
+  exit 1
+fi
+if [[ -z $CONTAINER_NAME ]]; then
+  echo "container name is not specified" >&2
+  exit 1
+fi
+if [[ -z $PREFIX ]]; then
+  echo "prefix is not specified" >&2
+  exit 1
+fi
+
 az storage blob list \
-  --connection-string $1 \
-  --container-name $2 \
-  --prefix $3 \
+  --connection-string $CONTAINER_STRING \
+  --container-name $CONTAINER_NAME \
+  --prefix $PREFIX \
   | jq -r '{"items": [sort_by(.properties.lastModified) | reverse | .[] | select(.name | endswith("/index.html") | not)] }' \
   | mustache /index.html.mustache \
   > index.html
+
 az storage blob upload \
-  --connection-string $1 \
-  --container-name $2 \
-  --name $3/index.html \
+  --connection-string $CONTAINER_STRING \
+  --container-name $CONTAINER_NAME \
+  --name $PREFIX/index.html \
   --file index.html
